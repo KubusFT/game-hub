a3db7e0a8d213f4cb3be3607c9e70de5266c9e21
Jakub Lademann <kubusft@protonmail.com>
2025-03-14 13:38:54 +0100

    fetch games

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import GameList from "./components/GameList";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
+        <GameList></GameList>
         </>
     );
 }
